a913dbe219f5309a608c7dda081b3b867e21a176
Divino Miranda <divinomiranda@yahoo.com.br>
2019-01-16 20:51:26 -0200

    Aula 97

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.ofertas = this.ofertasService.getOfertas()
-    console.log(this.ofertas)
+    //console.log(this.ofertas)
   }
 
 }
